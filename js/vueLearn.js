d8d3a17bdc9eaf7cc33ad0a8304db809f5674a84
SimpleLsd_One <liusungti@163.com>
2020-07-21 12:20:29 +0800

    Nothing

--- js/vueLearn.js
+++ js/vueLearn.js
@@ -1,6 +1,22 @@
-var app = new Vue({
-    el: '#app',
+var appList = new Vue({
+    el: '#appList',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        movies: ["星际穿越","黑暗骑士","钢铁之躯","盗梦空间"]
+    }
+})
+
+var appCounter = new Vue({
+    el: '#appCounter',
+    data: {
+        counter: 0
+    },
+    methods: {
+        add: function(){
+            this.counter++
+        },
+        sub: function(){
+            this.counter--
+        }
     }
 })
